53033e9236f05c71cc35e77c98ab894ebe79fae3
Carlos Faustino <chfcchfc96@gmail.com>
2024-02-26 09:48:11 -0300

    feat: add button to login

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -6,22 +6,31 @@ export default function Home() {
   return (
     <div className="bg-blue-gradient w-full h-dvh flex justify-center items-center">
       <div className="w-full max-w-screen-lg h-full max-h-[680px] flex items-start flex-col justify-between relative bg-landing bg-no-repeat bg-[80%_center]">
-        <Image
-          src="/logo.svg"
-          alt="Happy"
-          width={240}
-          height={74}
-        />
+        <div className="flex items-center gap-16">
+          <Image
+            src="/logo.svg"
+            alt="Happy"
+            width={240}
+            height={74}
+          />
+
+          <div className="text-xl flex flex-col leading-8">
+            <strong className="font-extrabold">São Paulo</strong>
+            <span className="font-semibold">São Paulo</span>
+          </div>
+        </div>
 
         <main className="max-w-80">
-          <h1 className="text-7xl font-black leading-[70px]">Leve felicidade para o mundo</h1>
+          <h1 className="text-7xl font-extrabold leading-[70px]">Leve felicidade para o mundo</h1>
           <p className="mt-10 text-2xl leading-8">Visite orfanatos e mude o dia de muitas crianças.</p>
         </main>
 
-        <div className="absolute right-0 top-0 text-2xl flex flex-col leading-8 text-right">
-          <strong className="font-extrabold">São Paulo</strong>
-          <span>São Paulo</span>
-        </div>
+        <Link
+          href="/"
+          className="absolute right-0 top-0 bg-blue-400 hover:bg-blue-300 py-3 px-10 flex items-center justify-center rounded-2xl transition-colors duration-200 font-extrabold text-white hover:text-blue-500 text-xl"
+        >
+          Acesso restrito
+        </Link>
 
         <Link
           href="/map"
