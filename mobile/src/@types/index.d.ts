6d9def665434b82f3a3dce62609ad61b077f3a5a
Carlos Faustino <chfcchfc96@gmail.com>
2024-02-29 11:54:45 -0300

    feat: add constants and typing

---
+++ mobile/src/@types/index.d.ts
@@ -0,0 +1,21 @@
+declare module "*.png";
+
+type Orphanage = {
+  id: string;
+  name: string;
+  about: string;
+  instructions: string;
+  openingHours: string;
+  openOnWeekends: boolean;
+  latitude: string;
+  longitude: string;
+  createdAt: Date;
+  updatedAt: Date;
+  images: Image[];
+};
+
+type Image = {
+  id: string;
+  path: string;
+  url: string;
+};
---
+++ mobile/src/constants/index.ts
@@ -0,0 +1,6 @@
+export const DEFAULT_POSITION = {
+  latitude: -23.4667111,
+  longitude: -46.5923237,
+  latitudeDelta: 0.014,
+  longitudeDelta: 0.014,
+};
