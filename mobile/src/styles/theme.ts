81a642b49eb2ad27791815ec18fc2c33fa5c82e8
Carlos Faustino <chfcchfc96@gmail.com>
2024-02-29 11:54:27 -0300

    feat: add theme

---
+++ mobile/src/styles/theme.ts
@@ -0,0 +1,40 @@
+export const THEME = {
+  COLORS: {
+    PRIMARY_600: "#0089A5",
+    PRIMARY_500: "#15C3D6",
+    PRIMARY_400: "#2AB5D1",
+    PRIMARY_300: "#96D2F0",
+    PRIMARY_200: "#B3DAE2",
+    PRIMARY_50: "#E6F7FB",
+
+    RED_500: "#FF669D",
+
+    GRAY_500: "#8FA7B3",
+    GRAY_300: "#CCCCCC",
+    GRAY_200: "#D3E2E5",
+    GRAY_100: "#F9FAFC",
+
+    GREEN_600: "#3CDC8C",
+    GREEN_500: "#37C77F",
+    GREEN_400: "#39CC83",
+    GREEN_200: "#A1E9C5",
+    GREEN_100: "#EDFFF6",
+
+    TEAL_400: "#5C8599",
+
+    WHITE: "#FFFFFF"
+  },
+  FONT_FAMILY: {
+    SEMI: "Nunito_600SemiBold",
+    BOLD: "Nunito_700Bold",
+    EXTRA: "Nunito_800ExtraBold",
+  },
+  SIZES: {
+    XS: 12,
+    SM: 14,
+    BASE: 16,
+    LG: 18,
+    XL: 20,
+    '2XL': 24
+  }
+};
