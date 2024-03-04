0ff4bd20e79954b995307ac04af4794370cc503f
Carlos Faustino <chfcchfc96@gmail.com>
2024-03-04 17:18:24 -0300

    refactor: update users routes

--- server/src/http/routes/users/index.ts
+++ server/src/http/routes/users/index.ts
@@ -6,53 +6,53 @@ import { GlobalError } from "@/core/global-error";
 import { User } from "@/domain/entities/user";
 import { bcryptHasher } from "@/http/helpers/bcrypt-hasher";
 import { prisma } from "@/lib/prisma";
-import { authenticate } from "@/plugins/authenticate";
-import { authorize } from "@/plugins/authorize";
 
-export async function usersRoutes(app: FastifyInstance) {
-  app.post("/",
-    {
-      config: { superUser: true },
-      onRequest: [authenticate],
-      preHandler: [authorize]
-    },
-    async (request, reply) => {
-      const createUserBody = z.object({
-        name: z.string().min(3),
-        email: z.string().email(),
-        password: z.string().refine(value => PASSWORD_REGEX.test(value), "Password must have minimum 8 characters, at least one lowercase letter, one uppercase letter, one number and one special character."),
-        confirmPassword: z.string()
+export async function usersRoutes(fastify: FastifyInstance) {
+  fastify.patch("/", async (request, reply) => {
+    const updateAccountQuery = z.object({
+      token: z.string().min(100),
+      email: z.string().email()
+    });
+
+    const updateAccountBody = z.object({
+      password: z.string().refine(value => PASSWORD_REGEX.test(value), "Password must have minimum 8 characters, at least one lowercase letter, one uppercase letter, one number and one special character."),
+      confirmPassword: z.string()
+    });
+
+    const { email, token } = updateAccountQuery.parse(request.query);
+
+    const user = await User.findUserByEmail(email);
+
+    if (!user || user.tokenResetPassword !== token) {
+      return GlobalError(reply, {
+        statusCode: 400,
+        error: "Bad Request",
+        message: "User not exists or invalid token."
       });
+    }
 
-      const { name, email, password, confirmPassword } = createUserBody.parse(request.body);
+    const { password, confirmPassword } = updateAccountBody.parse(request.body);
 
-      if (password !== confirmPassword) {
-        return GlobalError(reply, {
-          statusCode: 400,
-          error: "Bad Request",
-          message: "Password and confirm password don't match"
-        });
-      }
-
-      const exists = await User.findUserByEmail(email);
-
-      if (exists) {
-        return GlobalError(reply, {
-          statusCode: 400,
-          error: "Bad Request",
-          message: "User already exists"
-        });
-      }
-
-      const hashPassword = await bcryptHasher.hashGenerator(password);
-
-      await prisma.user.create({
-        data: {
-          name,
-          email,
-          password: hashPassword
-        }
+    if (password !== confirmPassword) {
+      return GlobalError(reply, {
+        statusCode: 400,
+        error: "Bad Request",
+        message: "Password and confirm password don't match."
       });
-    },
-  );
+    }
+
+    const hashPassword = await bcryptHasher.hashGenerator(password);
+
+    await prisma.user.update({
+      where: {
+        id: user.id
+      },
+      data: {
+        tokenResetPassword: null,
+        password: hashPassword
+      }
+    });
+
+    reply.code(200).send();
+  });
 }
