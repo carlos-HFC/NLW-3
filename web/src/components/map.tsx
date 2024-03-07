e52d0d9db4e73fd85accbdfa16e4b36bfaf34a2b
Carlos Faustino <chfcchfc96@gmail.com>
2024-03-07 13:26:20 -0300

    feat: get user location

--- web/src/components/map.tsx
+++ web/src/components/map.tsx
@@ -1,13 +1,33 @@
 "use client";
 
-import { Map as MapContainer, MapProps } from 'pigeon-maps';
+import { CircleIcon } from "lucide-react";
+import { Map as MapContainer, MapProps, Point } from 'pigeon-maps';
+import { useEffect, useState } from "react";
+
+import { Marker } from "./marker";
+
+const INITIAL_STATE_LOCATION = [-23.4667111, -46.5923237] as Point;
 
 export function Map(props: MapProps) {
+  const [location, setLocation] = useState<Point>(INITIAL_STATE_LOCATION);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(position => {
+      setLocation([position.coords.latitude, position.coords.longitude]);
+    });
+  }, []);
+
   return (
     <MapContainer
-      center={[-23.4667111, -46.5923237]}
+      center={location}
       {...props}
+      defaultZoom={15}
       attribution={false}
-    />
+    >
+      <Marker anchor={location}>
+        <CircleIcon className="size-5 stroke-2 fill-blue-500" />
+      </Marker>
+      {props.children}
+    </MapContainer>
   );
 }
--- web/src/components/marker.tsx
+++ web/src/components/marker.tsx
@@ -3,7 +3,7 @@
 import { ArrowRightIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { Draggable, Marker as MarkerContainer } from 'pigeon-maps';
+import { Marker as MarkerContainer } from 'pigeon-maps';
 import { ComponentProps, useEffect, useState } from "react";
 
 import { cn } from "@/utils";
@@ -11,13 +11,10 @@ import { cn } from "@/utils";
 interface PopupProps extends ComponentProps<typeof MarkerContainer> {
   href?: string;
   payload?: string;
-  draggable?: boolean;
 }
 
 export function Marker(props: PopupProps) {
   const [show, setShow] = useState(false);
-
-  const Popup = props.draggable ? Draggable : MarkerContainer;
 
   useEffect(() => {
     if (show) {
@@ -26,35 +23,37 @@ export function Marker(props: PopupProps) {
   }, [show]);
 
   return (
-    <Popup
-      className="!pointer-events-auto flex justify-center items-center gap-2"
-      width={64}
-      height={72}
+    <MarkerContainer
+      className={cn("!pointer-events-auto flex justify-center items-center gap-2 w-16", show ? "overflow-visible" : "overflow-hidden")}
       onClick={() => setShow(prev => !prev)}
       {...props}
     >
       <>
-        <Image
-          src="/map-marker.svg"
-          alt="Map marker"
-          width={64}
-          height={72}
-        />
+        {props.children ?? (
+          <Image
+            src="/map-marker.svg"
+            alt="Map marker"
+            width={64}
+            height={72}
+          />
+        )}
         {props.payload && (
           <div
-            className={cn("bg-white/80 min-w-60 rounded-2xl text-blue-600 text-xl font-bold flex justify-between items-center py-2 px-3 transition-opacity duration-300", show ? "opacity-100" : "opacity-0")}
+            className={cn("bg-white/80 min-w-60 w-full rounded-2xl text-blue-600 text-lg font-bold flex justify-between items-center gap-2 py-2 px-3 transition-opacity duration-300 absolute left-20", show ? "opacity-100" : "opacity-0")}
             onClick={e => e.stopPropagation()}
           >
-            {props.payload}
+            <p className="truncate">
+              {props.payload}
+            </p>
             <Link
               href={props.href ?? "/"}
-              className="size-10 bg-blue-500 rounded-xl flex justify-center items-center"
+              className="size-8 min-w-8 bg-blue-500 rounded-xl flex justify-center items-center"
             >
               <ArrowRightIcon className="size-5 stroke-white" />
             </Link>
           </div>
         )}
       </>
-    </Popup>
+    </MarkerContainer>
   );
 };
