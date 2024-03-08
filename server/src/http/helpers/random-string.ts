8b51c0e269eee3ee132621b17790ab68eb414d34
Carlos Faustino <chfcchfc96@gmail.com>
2024-03-08 13:57:42 -0300

    refactor: update random string function

--- server/src/http/helpers/random-string.ts
+++ server/src/http/helpers/random-string.ts
@@ -1,10 +1,5 @@
 import { randomBytes } from "node:crypto";
 
-type RandomStringParams = ({
-  size?: number;
-  encoding?: BufferEncoding;
-}) | undefined;
-
-export function randomString(params?: RandomStringParams) {
-  return randomBytes(params?.size ?? 50).toString(params?.encoding ?? 'hex').split('').sort(() => Math.random() - .5).join("");
+export function randomString(size: number = 50) {
+  return randomBytes(size).toString('hex').split('').sort(() => Math.random() - .5).join("");
 }
